a5dca2354903e7ea1de26bf9e9d9c7225f3794bf
vvasyanovich <vvasyanovich@plslogistics.com>
2012-10-14 03:50:18 +0300

    correct zIndex, add remove func for desktop

--- desktop.js
+++ desktop.js
@@ -6,10 +6,28 @@ function addFrame(frame)
 	length = desktop.frames.length;
 	desktop.frames[length] = frame;
 	length++;
-	frame.style.zIndex = length;
+	frame.style.zIndex = length-1;
 	document.body.appendChild(frame.box);
 	var activateFunction = function(event){activateFrame(event, frame);}
 	frame.titleBar.addEventListener('mousedown', activateFunction, false);
+	var closeFunc = function(event)
+	{
+		removeFrame(frame);		
+	}
+	frame.closeButton.addEventListener('click', closeFunc, false);
+}
+
+function removeFrame(frame)
+{
+	document.body.removeChild(frame.box);
+	length = desktop.frames.length;
+	index = desktop.frames.indexOf(frame);
+	for(var i = index+1; i < length; i++)
+	{
+		var currFrame = desktop.frames[i];
+		desktop.frames[i-1]=currFrame;
+	}	
+	desktop.frames.length = length-1;
 }
 
 function activateFrame(event, frame)
--- frame.js
+++ frame.js
@@ -1,23 +1,23 @@
-function createFrameTitleBarButtons(titleWidth, titleHeight)
+function createFrameTitleBarButtons(titleWidth, titleHeight, frame)
 {
 	var close = createDiv('closeFrameButton', 18, 18);
 	close.style.position = 'relative';
 	close.style.left = titleWidth - 19;
 	close.style.top = 1;
-	close.addEventListener('click', closeButton, false);
 	return close;
 }
 
 function closeButton(event)
 {
-	alert(1);
+	alert();
 }
 
 function createFrame(width, height, top, left) {
 	var frame = new Object();
 	frame.box = createDiv('box', width, height);
 	frame.titleBar = createDiv('title', width-4, 20);
-	var closeButton = createFrameTitleBarButtons(width-4, 20);
+	var closeButton = createFrameTitleBarButtons(width-4, 20, frame);
+	frame.closeButton = closeButton;
 	frame.titleBar.appendChild(closeButton);
 	frame.rootPanel = createDiv('rootPanel', width-4, height-26);
 	frame.style = frame.box.style;
